b6ac2c28fd38c4cc74650d7d44516a4ac1b4401d
Muratbek Bulganbayev <bulganbaev@gmail.com>
2016-01-19 20:16:57 +0600

    second project

---
+++ SECOND/SECOND/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECOND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,c;
+            string num;
+            Console.WriteLine("Enter your first number:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("What operation do you want to do?(*,/,+,-)");
+            num = Console.ReadLine();
+            c = 0;
+            if (num == "+")
+            {
+                c = a + b;
+            }
+            else if(num == "-")
+            {
+                c = a - b;
+            }
+            else if (num == "*")
+            {
+                c = a * b;
+            }
+            else if (num == "/")
+            {
+                c = a / b;
+            }
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
