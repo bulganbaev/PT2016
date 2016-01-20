501e495905b7d6db2868292ccb4ed899d21de869
Muratbek Bulganbayev <bulganbaev@gmail.com>
2016-01-20 13:03:10 +0600

    3rd

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -237,6 +237,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.equal);
             this.Controls.Add(this.sub);
